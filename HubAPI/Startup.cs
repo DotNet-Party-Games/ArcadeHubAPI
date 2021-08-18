b0ee7ecc0fca0cf066fc48d3d0f1cb56bec4819a
surajkalika2 <surajkalika2@gmail.com>
2021-08-18 17:17:17 -0400

    added initial cors need to add ingress address

--- HubAPI/Startup.cs
+++ HubAPI/Startup.cs
@@ -27,6 +27,20 @@ namespace HubAPI {
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HubAPI", Version = "v1" });
             });
+
+
+            services.AddCors(
+                (builder) =>
+                {
+                    builder.AddDefaultPolicy((policy) =>
+                            {
+                                policy.WithOrigins("")
+                                       .AllowAnyHeader()
+                                       .AllowAnyMethod();
+                            }
+                    );
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
