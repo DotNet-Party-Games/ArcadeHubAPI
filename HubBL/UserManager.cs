0cbf24de2056602666d72144316b0b0195a17943
syoung908 <syoung908@gmail.com>
2021-08-19 13:33:53 -0700

    Update UserManager.cs

---
+++ HubBL/UserManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HubDL;
+using HubEntities.Database;
+
+namespace HubBL {
+    public class UserManager {
+        private readonly IDatabase<User> _userDB;
+
+        public UserManager(IDatabase<User> userDB) {
+            _userDB = userDB;
+        }
+
+        public async Task<User> CreateUser(User user) {
+            return await _userDB.Create(user);
+        }
+
+        public async Task<bool> UpdateUser(User user) {
+            return await _userDB.Update(user);
+        }
+    }
+}
