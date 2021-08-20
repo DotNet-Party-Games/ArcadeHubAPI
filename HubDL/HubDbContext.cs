7e9120834fb4eaafdc0a578820d15a18cb8f36d5
syoung908 <syoung908@gmail.com>
2021-08-19 18:20:12 -0700

    database migration

--- HubDL/HubDbContext.cs
+++ HubDL/HubDbContext.cs
@@ -6,9 +6,23 @@ namespace HubDL {
         public HubDbContext(): base() { }
         public HubDbContext(DbContextOptions options) : base() { }
 
+        public DbSet<User> Users { get; set; }
+        public DbSet<Leaderboard> Leaderboards { get; set; }
+        public DbSet<TeamLeaderboard> TeamLeaderBoards { get; set; }
+        public DbSet<UserScore> UserScores { get; set; }
+        public DbSet<TeamScore> TeamScores { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TeamJoinRequest> TeamJoinRequests { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<User>();
-            modelBuilder.Entity<ChatMessage>();
+            //modelBuilder.Entity<ChatMessage>();
+            modelBuilder.Entity<Leaderboard>();
+            modelBuilder.Entity<TeamLeaderboard>();
+            modelBuilder.Entity<UserScore>();
+            modelBuilder.Entity<TeamScore>();
+            modelBuilder.Entity<Team>();
+            modelBuilder.Entity<TeamJoinRequest>();
         }
     }
 }
