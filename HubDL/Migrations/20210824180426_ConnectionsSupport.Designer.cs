1f3d737dcf480bace4e21d0a3d48ee05a49ca8a1
syoung908 <syoung908@gmail.com>
2021-08-24 11:36:47 -0700

    Connection table Migration

--- HubDL/Migrations/20210824180426_ConnectionsSupport.Designer.cs
+++ HubDL/Migrations/20210824180426_ConnectionsSupport.Designer.cs
@@ -3,14 +3,16 @@ using HubDL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HubDL.Migrations
 {
     [DbContext(typeof(HubDbContext))]
-    partial class HubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210824180426_ConnectionsSupport")]
+    partial class ConnectionsSupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
